e89eaef94975b55264f00ca3d84b5aa56e45a0e5
Beni Abazi <benabazi@gmail.com>
2025-06-30 12:54:37 +0200

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   src/pages/Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -56,16 +56,18 @@ const Contact = () => {
           <InfoText>Tuesday - Saturday: 9am - 6pm</InfoText>
           <InfoText>Sunday - Monday: Closed</InfoText>
         </InfoContainer>
-        <MapContainer>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.719833330862!2d-118.4013523847892!3d34.0736229806068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bc04d6a7e0f1%3A0x1de4dd4e1f36c4e5!2sRodeo%20Dr%2C%20Beverly%20Hills%2C%20CA%2090210!5e0!3m2!1sen!2sus!4v1625069 Rodeo Drive"
-            width="100%"
-            height="100%"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-          ></iframe>
-        </MapContainer>
+<MapContainer>
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.719833330862!2d-118.4013523847892!3d34.0736229806068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bc04d6a7e0f1%3A0x1de4dd4e1f36c4e5!2sRodeo%20Dr%2C%20Beverly%20Hills%2C%20CA%2090210!5e0!3m2!1sen!2sus!4v1625069"
+    width="100%"
+    height="100%"
+    style={{ border: 0 }}
+    allowFullScreen=""
+    loading="lazy"
+    title="Map showing location of LuxeLocks Hair Studio in Beverly Hills"
+  ></iframe>
+</MapContainer>
+
       </ContentWrapper>
       <div style={{ marginTop: '4rem' }}>
         <Title>Send us a Message</Title>
